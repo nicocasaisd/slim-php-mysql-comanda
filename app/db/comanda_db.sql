47f82672c937eee870453ab8c1ccfa22e98e7e33
nicocasaisd <nicolas.casais.dassie@gmail.com>
2023-06-12 20:16:06 -0300

    agrego tabla Orders

--- app/db/comanda_db.sql
+++ app/db/comanda_db.sql
@@ -40,28 +40,23 @@ DROP TABLE IF EXISTS `users`;
 
 CREATE TABLE
   `users` (
-    `id` int (11) NOT NULL,
+    `id` int (11) NOT NULL AUTO_INCREMENT,
     `user_name` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
     `password` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
     `user_type` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
-    `date_end` date DEFAULT NULL
+    `date_end` date DEFAULT NULL,
+    PRIMARY KEY (`id`)
   ) ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE = utf8_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `users`
 --
 INSERT INTO
-  `users` (
-    `id`,
-    `user_name`,
-    `password`,
-    `user_type`,
-    `date_end`
-  )
+  `users` (`user_name`, `password`, `user_type`, `date_end`)
 VALUES
-  (1, 'franco', 'Hsu23sDsjseWs', 'SOCIO', NULL),
-  (2, 'pedro', 'dasdqsdw2sd23', 'MOZO', NULL),
-  (3, 'jorge', 'sda2s2f332f2', 'BARTENDER', NULL);
+  ('franco', 'Hsu23sDsjseWs', 'SOCIO', NULL),
+  ('pedro', 'dasdqsdw2sd23', 'MOZO', NULL),
+  ('jorge', 'sda2s2f332f2', 'BARTENDER', NULL);
 
 --
 -- Estructura de tabla para la tabla `dishes`
@@ -83,8 +78,32 @@ CREATE TABLE
 INSERT INTO
   `dishes` (`name`, `type`, `price`)
 VALUES
-  ('Coca', 'BEBIDA',100),
+  ('Coca', 'BEBIDA', 100),
   ('Papas', 'COMIDA', 200);
+
+--
+-- Estructura de tabla para la tabla `orders`
+--
+DROP TABLE IF EXISTS `orders`;
+
+CREATE TABLE
+  `orders` (
+    `id` int (11) NOT NULL AUTO_INCREMENT,
+    `order_code` varchar(5) COLLATE utf8_unicode_ci NOT NULL,
+    `order_list` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
+    `order_price` decimal(10, 2) DEFAULT NULL,
+    `order_status` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+    PRIMARY KEY (`id`)
+  ) ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE = utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `orders`
+--
+INSERT INTO
+  `orders` (`order_code`, `order_list`, `order_price`, `order_status`)
+VALUES
+  ('ab100', "[{'coca':'2', 'bife':'1'}]", 300, 'EN PREPARACION'),
+   ('ab200', "[{'seven-up':'2', 'bife':'3'}]", 400, 'EN PREPARACION');
 
 --
 -- Índices para tablas volcadas
@@ -92,19 +111,16 @@ VALUES
 --
 -- Indices de la tabla `users`
 --
-ALTER TABLE `users` ADD PRIMARY KEY (`id`);
-
+-- ALTER TABLE `users` ADD PRIMARY KEY (`id`);
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 --
 -- AUTO_INCREMENT de la tabla `users`
 --
-ALTER TABLE `users` MODIFY `id` int (11) NOT NULL AUTO_INCREMENT,
-AUTO_INCREMENT = 4;
-
-COMMIT;
-
+-- ALTER TABLE `users` MODIFY `id` int (11) NOT NULL AUTO_INCREMENT,
+-- AUTO_INCREMENT = 4;
+-- COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
